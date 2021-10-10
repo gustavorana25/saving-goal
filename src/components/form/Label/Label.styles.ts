891c2ef84f09080b83e3eab3134f407ce9bbaff7
Gustavo Rana <gustavo.rana@agilize.com.br>
2021-10-10 10:59:53 -0300

    fix: same pattern for style only component and remove not used imports

---
+++ src/components/form/Label/Label.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  font-size: ${(props) => props.theme.fontSizes.small};
+  color: ${(props) => props.theme.colors.blueGray900};
+  display: block;
+  margin-bottom: ${(props) => props.theme.gutterSpace.xSmall};
+`;
